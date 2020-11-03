906fc2d06c6049452d6c93ef9cc788a60d29c932
Tiago Luz <tiago.piano@gmail.com>
2020-11-03 03:09:58 -0300

    Rotina de desmontagem e fixes.

--- trabalho/src/main/java/com/mycompany/trabalho/Desmontagem.java
+++ trabalho/src/main/java/com/mycompany/trabalho/Desmontagem.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.trabalho;
 
+import java.util.HashMap;
+
 /**
  *
  * @author tiagoluz
@@ -13,6 +15,7 @@ public class Desmontagem {
     
     String content;
     String contentDesmontado;
+    ParseHelper parseHelper;
     
     public Desmontagem(){}
     
@@ -21,14 +24,165 @@ public class Desmontagem {
         run();
     }
     
+    
+    
     public void run() {
-        this.contentDesmontado = this.content;
+        parseHelper = ParseHelper.getInstance();
+        String linhas[] = parseHelper.breakLines(content);
+        StringBuilder sb = new StringBuilder(".text\n");
+        
+        HashMap labels = new HashMap();
+        
+        int inicio = Integer.parseInt("400000",16); // inicio do programa 0x00400000
+        
+        for(int i = 0; i < linhas.length; i++) {
+            String linha = linhas[i].trim();
+            String binario = parseHelper.hexToBin(linha);
+            int opcode = parseHelper.binToInt(binario.substring(0, 6)); 
+            int func = parseHelper.binToInt(binario.substring(26, 32)); 
+            String instrucao = getInstrucao(opcode, func);
+            if(instrucao == null) {
+                System.out.println("Instrução inválida na linha " + (i + 1));
+                System.exit(-1);
+            }
+            sb.append(instrucao);
+            sb.append(" ");
+            String parametros = "";
+            String rs, rt, rd;
+            int offset, imediato;
+            
+            switch(opcode) {
+                case 0: 
+                    switch(func) {
+                        case 8: // jr
+                            rs = parseHelper.translateRegister(Integer.parseInt(binario.substring(6,11),2));
+                            parametros = rs;
+                        break;
+                        case 26: // div
+                            rs = parseHelper.translateRegister(Integer.parseInt(binario.substring(6,11),2));
+                            rt = parseHelper.translateRegister(Integer.parseInt(binario.substring(11,16),2));
+                            parametros = rs + "," + rt;
+                            break;
+                        case 39: // nor
+                            rs = parseHelper.translateRegister(Integer.parseInt(binario.substring(6,11),2));
+                            rt = parseHelper.translateRegister(Integer.parseInt(binario.substring(11,16),2));
+                            rd = parseHelper.translateRegister(Integer.parseInt(binario.substring(16,21),2));
+                            parametros = rd + "," + rs + "," + rt;
+                            break;
+                        case 0: // sll
+                            rt = parseHelper.translateRegister(Integer.parseInt(binario.substring(11,16),2));
+                            rd = parseHelper.translateRegister(Integer.parseInt(binario.substring(16,21),2));
+                            int shamt = parseHelper.trataOffset(binario.substring(21,26));
+                            parametros = rd + "," + rt + "," + shamt;
+                            break;
+                        default:
+                            System.out.println("O código de função da instrução informado é inválido ou não foi implementado. (opcode: "+opcode+" func: "+func+") na linha " + (i+1));
+                            System.exit(-1);
+                    }
+                    break;
+                case 3: // jal
+                    offset = (Integer.parseInt(binario.substring(7)+"00",2) - inicio) / 4;
+                    parametros = verificaLabel(labels, i+1, offset);
+                    break;
+                case 4: // beq
+                    rs = parseHelper.translateRegister(Integer.parseInt(binario.substring(6,11),2));
+                    rt = parseHelper.translateRegister(Integer.parseInt(binario.substring(11,16),2));
+                    offset = parseHelper.trataOffset(binario.substring(16,32));
+                    parametros = rs + "," + rt + "," + verificaLabel(labels, i+1, offset);   
+                    break;
+                case 35: // lw
+                    rs = parseHelper.translateRegister(Integer.parseInt(binario.substring(6,11),2));
+                    rt = parseHelper.translateRegister(Integer.parseInt(binario.substring(11,16),2));
+                    offset = parseHelper.trataOffset(binario.substring(16,32));
+                    parametros = rt + "," + offset + "("+rs+")";
+                    break;
+                case 6: // blez
+                    rs = parseHelper.translateRegister(Integer.parseInt(binario.substring(6,11),2));
+                    rt = parseHelper.translateRegister(Integer.parseInt(binario.substring(11,16),2));
+                    offset = parseHelper.trataOffset(binario.substring(16,32));
+                    parametros = rt + "," + verificaLabel(labels, i+1, offset);   
+                    break;
+                case 13: // ori
+                    rs = parseHelper.translateRegister(Integer.parseInt(binario.substring(6,11),2));
+                    rt = parseHelper.translateRegister(Integer.parseInt(binario.substring(11,16),2));
+                    imediato = parseHelper.trataOffset(binario.substring(16,32));
+                    parametros = rt + "," + rs + "," + imediato;
+                    break;
+                case 10: // slti
+                    rs = parseHelper.translateRegister(Integer.parseInt(binario.substring(6,11),2));
+                    rt = parseHelper.translateRegister(Integer.parseInt(binario.substring(11,16),2));
+                    imediato = parseHelper.trataOffset(binario.substring(16,32));
+                    parametros = rt + "," + rs + "," + imediato;
+                    break;
+                default:
+                    System.out.println("O opcode da instrução informado é inválido ou não foi implementado. (opcode: "+opcode+") na linha " + (i+1));
+                    System.exit(-1);
+            }
+            
+            sb.append(parametros);
+            sb.append("\n");
+        }
+        
+        // adiciona labels
+        StringBuilder sb2 = new StringBuilder();
+        String ls[] = sb.toString().split("\n");
+        for(int i = 0; i < ls.length; i++) {
+            if(labels.containsKey(i)) {
+                ls[i] = labels.get(i)+":"+ls[i];
+            }
+            sb2.append(ls[i]);
+            sb2.append("\n");
+        }
+        
+        this.contentDesmontado = sb2.toString();
     }
     
-    
-
-     @Override
+    @Override
     public String toString() {
         return this.contentDesmontado;
+    }
+
+    private String getInstrucao(int opcode, int func) {
+        if(opcode == 0) {
+            switch(func) {
+                case 39:
+                    return "nor";
+                case 8:
+                    return "jr";
+                case 0:
+                    return "sll";
+                case 26: 
+                    return "div";
+                default:
+                    return null;
+            }
+        } else {
+            switch(opcode) {
+                case 3:
+                    return "jal";
+                case 10:
+                    return "slti";
+                case 35:
+                    return  "lw";
+                case 4:
+                    return "beq";
+                case 6:
+                    return "blez";
+                case 13: 
+                    return "ori";
+                default:
+                    return null;
+            }
+        }
+    }
+
+    private String verificaLabel(HashMap labels,int linhaAtual, int offset) {
+        int destino = linhaAtual + offset;
+        if(labels.containsKey(destino)) {
+            return labels.get(destino).toString();
+        } else {
+            labels.put(destino, "label"+destino);
+            return "label"+destino;
+        }
     }
 }
--- trabalho/src/main/java/com/mycompany/trabalho/ParseHelper.java
+++ trabalho/src/main/java/com/mycompany/trabalho/ParseHelper.java
@@ -6,6 +6,7 @@
 package com.mycompany.trabalho;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  *
@@ -14,6 +15,7 @@ import java.util.HashMap;
 public class ParseHelper {
     
     static HashMap regTable;
+    static HashMap regTableInd;
     
     private static ParseHelper instance;
     
@@ -23,6 +25,8 @@ public class ParseHelper {
     
     static {
         regTable = new HashMap();
+        regTableInd = new HashMap();
+        
         regTable.put("$zero", 0);
         regTable.put("$at", 1);
         regTable.put("$v0", 2);
@@ -55,8 +59,40 @@ public class ParseHelper {
         regTable.put("$sp", 29);
         regTable.put("$fp", 30);
         regTable.put("$ra", 31);
+        regTableInd.put(0,"$zero");
+        regTableInd.put(1,"$at");
+        regTableInd.put(2,"$v0");
+        regTableInd.put(3,"$v1");
+        regTableInd.put(4,"$a0");
+        regTableInd.put(5,"$a1");
+        regTableInd.put(6,"$a2");
+        regTableInd.put(7,"$a3");
+        regTableInd.put(8,"$t0");
+        regTableInd.put(9,"$t1");
+        regTableInd.put(10,"$t2");
+        regTableInd.put(11,"$t3");
+        regTableInd.put(12,"$t4");
+        regTableInd.put(13,"$t5");
+        regTableInd.put(14,"$t6");
+        regTableInd.put(15,"$t7");
+        regTableInd.put(16,"$s0");
+        regTableInd.put(17,"$s1");
+        regTableInd.put(18,"$s2");
+        regTableInd.put(19,"$s3");
+        regTableInd.put(20,"$s4");
+        regTableInd.put(21,"$s5");
+        regTableInd.put(22,"$s6");
+        regTableInd.put(23,"$s7");
+        regTableInd.put(24,"$t8");
+        regTableInd.put(25,"$t9");
+        regTableInd.put(26,"$k0");
+        regTableInd.put(27,"$k1");
+        regTableInd.put(28,"$gp");
+        regTableInd.put(29,"$sp");
+        regTableInd.put(30,"$fp");
+        regTableInd.put(31,"$ra");
     }
-    
+ 
     public static ParseHelper getInstance() {
         if(instance == null) {
             instance = new ParseHelper();
@@ -71,6 +107,10 @@ public class ParseHelper {
     
     public int translateRegister(String reg) {
         return (int)regTable.get(reg.trim());
+    }
+    
+    public String translateRegister(int ind) {
+        return regTableInd.get(ind).toString();
     }
     
     public String padLeft(String inputString, int length, char pad) {
@@ -105,5 +145,30 @@ public class ParseHelper {
             sb.append(Integer.toHexString(h));
         }
         return "0x"+sb.toString();
+    }
+
+    int binToInt(String val) {
+        return Integer.parseInt(val,2);
+    }
+     
+    String hexToBin(String val) {
+        StringBuilder sb = new StringBuilder();
+        if(val.startsWith("0x")) {
+            val = val.substring(2);
+        }
+        for(int i = 0; i < val.length(); i++) {
+            int h = Integer.parseInt(String.valueOf(val.charAt(i)), 16);
+            sb.append(padLeftZeros(intToBin(h), 4));
+        }
+        return sb.toString();
+    }
+
+    int trataOffset(String binario) {
+        int i = Integer.parseInt(binario,2);
+        if(binario.startsWith("1")) {
+            return i - 65535;
+        } else {
+            return i;
+        }
     }
 }
